0427c74a4e76ea9d52479178e837d505929700e4
maxbright <mab472@comcast.net>
2014-01-12 16:30:26 -0800

    Added number support, sounds now stored in memory

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,5 @@
 //Text To Max
-//Copyright 2013 Max Bright. All Rights Reserved.
+//Copyright 2014 Max Bright. All Rights Reserved.
 
 #include <iostream>
 #include <string>
@@ -7,38 +7,83 @@
 
 using namespace std;
 
+void playLetter(char);
+void playString(string);
+
 void playLetter(char input)
 {
-	LPCSTR soundFile;
-	string soundFileString;
-	bool useSoundFile = true;
+	//Convert char input to LPCSTR
+	//There needs to be a better way to do this
+	LPCSTR soundName;
+	string inputString;
+	inputString = input;
+	soundName = inputString.c_str();
 
-	soundFileString = "resource/ .wav";
-
+	//If upper case, convert input to lower case and run the function again
 	if (65 <= input && input <= 90)
 	{
 		input += 32;
-		useSoundFile = false;
 		playLetter(input);
 	}
-	else if (input == ' ')
+	else if (input == ' ' || input == ',' || input == '.')
 	{
-		Sleep(100);
-		useSoundFile = false;
+		Sleep(75);
 	}
+	else if (48 <= input && input <= 57)
+	{
+		switch(input)
+		{
+		case '0':
+			playString("zero");
+			break;
+		case '1':
+			playString("one");
+			break;
+		case '2':
+			playString("two");
+			break;
+		case '3':
+			playString("three");
+			break;
+		case '4':
+			playString("four");
+			break;
+		case '5':
+			playString("five");
+			break;
+		case '6':
+			playString("six");
+			break;
+		case '7':
+			playString("seven");
+			break;
+		case '8':
+			playString("eight");
+			break;
+		case '9':
+			playString("nine");
+			break;
+		default:
+			//'!' will play the error message
+			playString("!");
+		}
+	}
+	//If on a-z set
 	else if (97 <= input && input <= 122)
 	{
-		soundFileString[9] = input;
+		PlaySound(soundName, GetModuleHandle(0), SND_RESOURCE);
 	}
 	else
 	{
-		soundFileString = "resource/unrecognizedinput.wav";
+		PlaySound("error", GetModuleHandle(0), SND_RESOURCE);
 	}
+}
 
-	if (useSoundFile)
+void playString(string input)
+{
+	for (int i = 0; i < input.length(); i++)
 	{
-		soundFile = soundFileString.c_str();
-		PlaySound(TEXT(soundFile), NULL, SND_FILENAME);
+		playLetter(input[i]);
 	}
 }
 
@@ -46,20 +91,9 @@ int main()
 {
 	cout << "Welcome to TextToMax, type in something and listen to your favorite voice actor reading it out to you!\n";
 	string input = "None";
-	bool running = true;
-	while (running)
+	while (true)
 	{
 		getline(cin, input);
-		if (input == "quit")
-		{
-			running = false;
-		}
-		else
-		{
-			for (int i = 0; i < input.length(); i++)
-			{
-				playLetter(input[i]);
-			}
-		}
+		playString(input);
 	}
 }
